94a987693cf9352fdd59af873e364ab7b6b8035b
spurekarcareer <spurekarcareer@users.noreply.github.com>
2013-10-24 14:53:03 -0400

    Mid-game check in. Game objects created, next step is to draw them on the screen

--- pong.js
+++ pong.js
@@ -10,4 +10,44 @@ window.onload = function() {
     canvas.width = W;
     canvas.height = H;
     ctx.fillRect(0,0,W,H);
+
+    //Initialize game pieces
+    var particles = [],
+        ball = {},
+        paddles = [2];
+   
+
+    //Ball object
+    ball = {
+        x: 50,
+        y: 50,
+        r: 5,
+        c: "white",
+        vx: 4,
+        vy: 8,
+
+    //Function for drawing the ball on canvas
+    draw: function() {
+        ctx.beginPath();
+        ctx.fillStyle = this.c;
+        ctx.arc(this.x, this.y, this.r, 0, Math.PI*2, false);
+        //parameters above are: x init coord, y init coord, arc radius, starting angle, final angle, cw (ccw)
+        ctx.fill();
+    }
+    };
+
+    //Paddle object
+    function paddle(pos) {
+        //height and width
+        this.h = 5;
+        this.w = 150;
+
+        //position
+        this.x = W/2 - this.w/2;
+        this.y = (pos == "top") ? 0 : H - this.h;
+    };
+
+    //Initialize new paddles
+    paddles.push(new paddle("bottom"));
+    paddles.push(new paddle("top"));
 };
