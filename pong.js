7fba83db09462e9a3e4c93aec431cd9c3630a502
spurekarcareer <spurekarcareer@users.noreply.github.com>
2013-10-25 00:19:44 -0400

    Working game. Still need to clear screen on game over

--- pong.js
+++ pong.js
@@ -34,10 +34,9 @@ var mouse = {},
     points = 0, //current score
     init,
     over = 0, //set to 1 when game ends
+    startBtn = {},
+    restartBtn = {},
     paddleHit;
-
-//Note mouse movements
-canvas.addEventListener("mousemove", trackPosition, true);
 
 //Canvas properties
 canvas.width = W;
@@ -48,21 +47,6 @@ function paintCanvas() {
     ctx.fillStyle = "navy";
     ctx.fillRect(0,0,W,H);
 }
-
-//Paddle object
-function Paddle(pos) {
-    //height and width
-    this.h = 5;
-    this.w = 150;
-
-    //position
-    this.x = W/2 - this.w/2;
-    this.y = (pos == "top") ? 0 : H - this.h;
-}
-
-//Initialize new paddles
-paddles.push(new Paddle("bottom"));
-paddles.push(new Paddle("top"));
 
 //Ball object
 ball = {
@@ -77,29 +61,96 @@ ball = {
     draw: function() {
         ctx.beginPath();
         ctx.fillStyle = this.c;
-        ctx.arc(this.x, this.y, this.r, 0, Math.PI*2, false);
+        var cw = Math.random();
+        if (cw < 0.5) {
+            cw = true;
+        }
+        else {
+            cw = false;
+        };
+
+        ctx.arc(this.x, this.y, this.r, 0, Math.PI*2, cw);
         //parameters above are: x init coord, y init coord, arc radius, starting angle, final angle, cw (ccw)
         ctx.fill();
     }
 };
+
+//Paddle object
+function Paddle(pos) {
+    //height and width
+    this.h = 5;
+    this.w = 150;
+
+    //position
+    this.x = W/2 - this.w/2;
+    this.y = (pos == "top") ? 0 : H - this.h;
+}
+
+//Start button object
+startBtn = {
+    w: 100,
+    h: 50,
+    x: W/2 - 50,
+    y: H/2 - 25,
+
+    draw: function() {
+        ctx.strokeStyle = "yellow";
+        ctx.lineWidth = "2";
+        ctx.strokeRect(this.x,this.y,this.w,this.h);
+
+        ctx.font = "18px Arial, sans-serif";
+		ctx.textAlign = "center";
+		ctx.textBaseline = "middle";
+		ctx.fillStlye = "white";
+		ctx.fillText("Start", W/2, H/2 );
+    }
+};
+
+// Restart Button object
+restartBtn = {
+	w: 100,
+	h: 50,
+	x: W/2 - 50,
+	y: H/2 - 50,
+	
+	draw: function() {
+		ctx.strokeStyle = "yellow";
+		ctx.lineWidth = "2";
+		ctx.strokeRect(this.x, this.y, this.w, this.h);
+		
+		ctx.font = "18px Arial, sans-serif";
+		ctx.textAlign = "center";
+		ctx.textBaseline = "middle";
+		ctx.fillStlye = "white";
+		ctx.fillText("Restart", W/2, H/2 - 25 );
+	}
+};
+
+//Initialize new paddles
+paddles.push(new Paddle("bottom"));
+paddles.push(new Paddle("top"));
+
+//Note mouse movements
+canvas.addEventListener("mousemove", trackPosition, true);
+canvas.addEventListener("mousedown", btnClick, true);
+
+//Function to track mouse position
+function trackPosition(e) {
+    mouse.x = e.pageX;
+    mouse.y = e.pageY;
+}
 
 //Draw everything on canvas
 function draw() {
     paintCanvas();
     for (var i=0; i < paddles.length; i++) {
         p = paddles[i];
-        ctx.fillStyle = "white";
+        ctx.fillStyle = "yellow";
         ctx.fillRect(p.x,p.y,p.w,p.h);
     };
 
     ball.draw();
     update();
-}
-
-//Function to track mouse position
-function trackPosition(e) {
-    mouse.x = e.pageX;
-    mouse.y = e.pageY;
 }
 
 //Function for running the animation
@@ -108,10 +159,11 @@ function animLoop() {
     draw();
 };
 
-animLoop();
-
 //Function to update the screen (main for this program)
 function update() {
+
+    //Display score
+    updateScore();
 
     //Move paddles with mouse
     if (mouse.x && mouse.y) {
@@ -200,6 +252,16 @@ function collideAction(ball, p) {
 
 };
 
+//Handle scoring
+function updateScore() {
+    ctx.fillStyle = "white";
+    ctx.font = "16px Arial, sans-serif";
+	ctx.textAlign = "left";
+	ctx.textBaseline = "top";
+	ctx.fillText("Score: " + points, 20, 20 );
+}
+
+//Handle game ending
 function gameOver() {
     ctx.fillStyle = "white";
     ctx.font = "20px Arial, sans-serif";
@@ -212,5 +274,46 @@ function gameOver() {
 
     //set a game over flag
     over = 1;
+
+    //Draw the restart button
+    restartBtn.draw();
+
 };
+
+//start screen
+function startScreen() {
+    draw();
+    startBtn.draw();
+};
+
+//Handle button clicked
+function btnClick(e) {
+    //Store mouse positions
+    var mx = e.pageX,
+        my = e.pageY;
+
+    //Click start button
+    if(mx >= startBtn.x && mx <= startBtn.x + startBtn.w) {
+        //start the animation
+        animLoop();
+    }
+
+    //Click restart button
+    if(over == 1) {
+        if(mx >= restartBtn.x && mx <= restartBtn.x + restartBtn.w) {
+            ball.x = 20;
+            ball.y = 20;
+            ball.vx = 4;
+            ball.vy = 8;
+            points = 0;
+            over = 0;
+
+            //start the animation
+            animLoop();
+        }
+    }
+}
+
+//Display start screen
+startScreen();
 
